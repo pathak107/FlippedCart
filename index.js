f07347d70d6fcd95141efd2e0b0ae1a9a568f545
Shubham <smartshubhampathak@gmail.com>
2020-02-24 19:17:31 +0530

    New Jquery file to send http requests

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  app.set('view engine', 'ejs');
  app.use(express.static('public'));
 
-
  app.get('/', (req, res) => {
     res.render('index.ejs');
  });
---
+++ public/js/httpReqRes.js
@@ -0,0 +1,9 @@
+// var userID = -1;
+
+// $(".site-logo")
+// $(".site-logo").click(function(){
+//     $.get("/", function(data){
+//       userID= data;
+//       alert("user id is "+ userID);
+//     });
+// });
