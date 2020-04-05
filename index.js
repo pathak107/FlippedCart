569d82a06c49904d730203ac94344d07764707ac
Shubham <smartshubhampathak@gmail.com>
2020-04-06 02:14:53 +0530

    Connected with sqlite instead of mysql

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const connection = require('./Database/connection.js');
 const session = require('express-session')
+
+const bcrypt= require('bcrypt');
+const saltRounds=10;
+
+var sqlite3 = require('sqlite3').verbose()
+var db = new sqlite3.Database('./Database.db',(err)=>{
+   if(err) console.log(err);
+   console.log("Connected to Database");
+})
+
 
 
 app.set('view engine', 'ejs');
@@ -22,71 +31,144 @@ app.use(session({
 
 //Home Route
 app.get('/', (req, res) => {
-   connection.query('SELECT * from products', function (error, results, fields) {
+   db.all('SELECT * from Products', function (error, results) {
       if (error) console.log(error);
       res.render('index.ejs', { products: results });
-      // console.log(results.length);
-
    });
-
 });
 
 
 //Register Route
 app.route('/Register')
-   .get(function (req, res) {
-      res.render('LoginRegister.ejs');
-   })
-   .post(function (req, res) {
-      var query1 = 'INSERT INTO customer (userName,pass) VALUES (?,?)';
-      connection.query(query1, [req.body.userName, req.body.userPassword], function (error, results, fields) {
-         if (error) console.log(error);
-         console.log("Inserted into Database");
-      });
-
-      var query2 = 'select count(custID) as userID from customer';
-      connection.query(query2, function (error, results, fields) {
-         if (error) console.log(error);
-         req.session.userID = results[0].userID;
-         req.session.save();
-         // console.log(req.session.userID);
-      });
-
-   });
+    .get((req, res) => {
+        res.render('register.ejs');
+    })
+    .post((req, res) => {
+        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+            // Store hash in your password DB.
+            if (err) throw err;
+            var query1 = 'insert into Customer (UserName,Password) values (?,?);';
+            db.run(query1, [req.body.userName, hash], (error) => {
+                if (error) throw error;
+                console.log('Value inserted successfuly');
+            });
+            db.get('SELECT count(*) as custID from Customer', function (error, result) {
+                if (error) throw error;
+                req.session.cust_id = result.custID;
+                console.log("Customer id is " + req.session.cust_id);
+                res.redirect('/');
+            });
+        });
+    });
 
 //Login Route
 app.route('/Login')
-   .get(function (req, res) {
-      res.render('LoginRegister.ejs');
-   })
-   .post(function (req, res) {
-      var query1 = 'SELECT custID from customer where userName=? AND pass=?';
-      connection.query(query1, [req.body.userName, req.body.userPassword], function (error, results, fields) {
-         if (error) console.log(error);
-         else if (results[0]==null) res.render('LoginRegister.ejs');
-         else 
-         { req.session.userID = results[0].custID;
-            req.session.save();
-            console.log(req.session.userID);
-         }
-      });
-
-   });
+    .get((req, res) => {
+        res.render('login.ejs', { loginStatus: "Enter to Authenticate" });
+    })
+    .post((req, res) => {
 
 
+        var query1 = 'select cust_id,Password from Customer where UserName =?;';
+        db.get(query1, [req.body.userName], (error, row) => {
+            if (row == undefined)
+                res.render('login.ejs', { loginStatus: "Wrong Email or Password. Try Again! " });
+
+            else if(error) throw error;
+
+            else {
+                bcrypt.compare(req.body.password, row.Password, function (err, result) {
+                    // result == true
+                    if (result == true) {
+                        console.log('Authenticated successfully');
+                        console.log(row.cust_id);
+                        req.session.cust_id = row.cust_id;
+                        res.redirect('/');
+                    }
+                    else {
+                        res.render('login.ejs', { loginStatus: "Wrong Email or Password. Try Again! " });
+                    }
+                });
+            }
+
+
+
+        });
+});
+
+
+//Cart route
 app.get('/cart', (req, res) => {
    res.render('cart.ejs');
-   console.log(req.session.userID);
+   console.log(req.session.cust_id);
 });
+app.post('/cart',(req,res)=>{
+   if(req.session.cust_id==undefined)
+   {
+      res.json({message:"unauthorized"});
+   }
+   else{
+      db.run("insert into CartItem(cust_id,p_id) values (?,?)",[req.session.cust_id,req.body.p_id],(err)=>{
+         if(err) console.log(err);
+         console.log("Item inserted in Cart");
+         res.sendStatus(200);
+      })
+   }
+   
+});
+
+
+
+//Category Routes
 app.get('/category', (req, res) => {
-   res.render('category.ejs');
+   var products;
+   var category;
+   db.serialize(()=>{  
+      db.all('SELECT * from Products', function (error, results) {
+         if (error) console.log(error);
+         products=results;
+      });
+      db.all('SELECT * from Category', function (error, results) {
+         if (error) console.log(error);
+         category=results;   
+         res.render('category.ejs', { category:category,products:products });
+      });
+      
+   })
 });
+app.use('/category/:cat_id',express.static('public'));
+app.get('/category/:cat_id',(req,res)=>{
+   var products;
+   var category;
+   db.serialize(()=>{  
+      db.all("SELECT * from Products where cat_id=?",[req.params.cat_id] , (error, results)=> {
+         if (error) console.log(error);
+         products=results;
+      });
+      db.all('SELECT * from Category', function (error, results) {
+         if (error) console.log(error);
+         category=results;   
+         res.render('category.ejs', { category:category,products:products });
+      });
+      
+   })
+});
+
 app.get('/checkout', (req, res) => {
    res.render('checkout.ejs');
 });
-app.get('/product', (req, res) => {
-   res.render('product.ejs');
+
+//Products route
+app.use('/product/:p_id',express.static('public'));
+app.get('/product/:p_id',(req,res)=>{
+   db.get('select * from Products where p_id=?',[req.params.p_id],(err,row)=>{
+      if(err)console.log(err);
+      res.render('product.ejs',{product:row});
+   });
 });
+
+
+
 app.get('/contact', (req, res) => {
    res.render('contact.ejs');
 });
